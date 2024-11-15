78ec96a5c25e3206c471fd22452750d3987824a7
Finn Bear <finnbearlabs@gmail.com>
2024-11-14 21:17:22 -0800

    Better confetti.

--- pages/src/main.rs
+++ pages/src/main.rs
@@ -1,17 +1,101 @@
-use yew::{function_component, html, Html};
-use yew_confetti::Confetti;
+use std::ops::Deref;
+use std::str::FromStr;
+use web_sys::HtmlInputElement;
+use yew::{
+    function_component, html, use_state_eq, Callback, Html, InputEvent, Properties, TargetCast,
+};
+use yew_confetti::{Confetti, ConfettiProps};
 
 #[function_component(App)]
 fn app() -> Html {
-    html! {
+    let props = use_state_eq(|| {
+        let __yew_props = ConfettiProps::builder();
+        let __yew_required_props_token = ::yew::html::AssertAllProps;
+        ::yew::html::Buildable::prepare_build(__yew_props, &__yew_required_props_token).build()
+    });
+
+    let slider_factory = {
+        let props = props.clone();
+        move |name,
+              min: f32,
+              max: f32,
+              load: fn(&ConfettiProps) -> f32,
+              store: fn(&mut ConfettiProps, f32)|
+              -> Html {
+            let props = props.clone();
+            let value = load(&*props).to_string();
+            let new_props = props.deref().clone();
+            let oninput = Callback::from(move |event: InputEvent| {
+                let input = event.target_dyn_into::<HtmlInputElement>().unwrap();
+                let value = input.value();
+                let mut new_props = new_props.clone();
+                store(&mut new_props, f32::from_str(&value).unwrap());
+                props.set(new_props);
+            });
+            html! {<tr>
+                <td>{name}{":"}</td>
+                <td>
+                    <input
+                        type="range"
+                        min={min.to_string()}
+                        max={max.to_string()}
+                        step="0.001"
+                        value={value.clone()}
+                        {oninput}
+                    />
+                </td>
+                <td>{value}</td>
+            </tr>}
+        }
+    };
+
+    html! {<>
         <Confetti
-            scalar={20.0}
-            x={100.0}
-            y={50.0}
-            gravity={-9.81}
-            start_velocity={20.0}
+            style={"background-color: black;"}
+            ..props.deref().clone()
         />
-    }
+        <table style="border-spacing: 0.25rem; table-layout: fixed; border-collapse: separate;">
+            {slider_factory("width", 64.0, 512.0, |props| props.width as f32, |props, width| {
+                props.width = width as u32;
+            })}
+            {slider_factory("height", 64.0, 512.0, |props| props.height as f32, |props, height| {
+                props.height = height as u32;
+            })}
+            {slider_factory("count", 1.0, 500.0, |props| props.count as f32, |props, count| {
+                props.count = count as usize;
+            })}
+            {slider_factory("x", -0.1, 1.1, |props| props.x, |props, x| {
+                props.x = x;
+            })}
+            {slider_factory("y", -0.1, 1.1, |props| props.y, |props, y| {
+                props.y = y;
+            })}
+            {slider_factory("angle", 0.0, std::f32::consts::TAU, |props| props.angle, |props, angle| {
+                props.angle = angle;
+            })}
+            {slider_factory("spread", 0.0, std::f32::consts::PI, |props| props.spread, |props, spread| {
+                props.spread = spread;
+            })}
+            {slider_factory("velocity", 0.1, 3.0, |props| props.velocity, |props, velocity| {
+                props.velocity = velocity;
+            })}
+            {slider_factory("decay", 0.001, 1.0, |props| props.decay, |props, decay| {
+                props.decay = decay;
+            })}
+            {slider_factory("drift", -1.0, 1.0, |props| props.drift, |props, drift| {
+                props.drift = drift;
+            })}
+            {slider_factory("gravity", 0.0, 2.0, |props| props.gravity, |props, gravity| {
+                props.gravity = gravity;
+            })}
+            {slider_factory("lifespan", 1.0, 5.0, |props| props.lifespan, |props, lifespan| {
+                props.lifespan = lifespan;
+            })}
+            {slider_factory("scalar", 0.1, 10.0, |props| props.scalar, |props, scalar| {
+                props.scalar = scalar;
+            })}
+        </table>
+    </>}
 }
 
 fn main() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 use js_sys::wasm_bindgen::{prelude::Closure, JsCast};
 use web_sys::{window, CanvasRenderingContext2d, HtmlCanvasElement};
 use yew::{
@@ -11,41 +13,55 @@ pub struct ConfettiProps {
     pub width: u32,
     #[prop_or(256)]
     pub height: u32,
+    /// If continuous, controls max alive particles. Otherwise, controls how many spawn at beginning.
     #[prop_or(50)]
-    pub particle_count: u32,
-    #[prop_or((-90f32).to_radians())]
+    pub count: usize,
+    /// Emitter horizontal position. 0.0 means left edge, 1.0 means right edge.
+    #[prop_or(0.5)]
+    pub x: f32,
+    /// Emitter vertical position. 0.0 means bottom edge, 1.0 means top edge.
+    #[prop_or(0.5)]
+    pub y: f32,
+    /// Launch angle (0 = right, PI/2 = up, etc.)
+    #[prop_or(90f32.to_radians())]
     pub angle: f32,
+    /// Random variation in launch angle (PI/2 = PI/4 on each side)
     #[prop_or(45f32.to_radians())]
     pub spread: f32,
-    #[prop_or(45.0)]
-    pub start_velocity: f32,
-    #[prop_or(0.9)]
+    #[prop_or(2.0)]
+    pub velocity: f32,
+    /// Velocity decay per second.
+    #[prop_or(0.25)]
     pub decay: f32,
+    /// Downward acceleration.
     #[prop_or(1.0)]
     pub gravity: f32,
+    /// Rightward acceleration.
     #[prop_or(0.0)]
     pub drift: f32,
     /// Number of seconds each particle lasts.
     #[prop_or(4.0)]
     pub lifespan: f32,
-    #[prop_or(0.5)]
-    pub x: f32,
-    #[prop_or(0.5)]
-    pub y: f32,
+    #[prop_or(true)]
+    pub continuous: bool,
     /// Shape probability distribution. Repeated shapes are more likely.
     #[prop_or(&[Shape::Circle, Shape::Square])]
     pub shapes: &'static [Shape],
     /// CSS color probability distribution. Repeated colors are more likely.
     #[prop_or(&["#26ccff", "#a25afd", "#ff5e7e", "#88ff5a", "#fcff42", "#ffa62d", "#ff36ff"])]
     pub colors: &'static [&'static str],
+    /// Don't show any confetti if user prefers reduced motion, according to a CSS media query.
     #[prop_or(true)]
     pub disable_for_reduced_motion: bool,
-    #[prop_or(1.0)]
+    #[prop_or(5.0)]
     pub scalar: f32,
+    /// Classes to apply to the canvas.
     #[prop_or_default]
     pub class: Classes,
+    /// Inline style to apply to the canvas.
     #[prop_or(None)]
     pub style: Option<AttrValue>,
+    /// Id of the canvas.
     #[prop_or(None)]
     pub id: Option<AttrValue>,
 }
@@ -61,6 +77,7 @@ fn request_animation_frame(f: &Closure<dyn FnMut(f64)>) {
 struct State {
     confetti: Vec<Fetti>,
     callback: Option<Closure<dyn FnMut(f64)>>,
+    last_time: f64,
 }
 
 #[function_component(Confetti)]
@@ -80,18 +97,22 @@ pub fn confetti(props: &ConfettiProps) -> Html {
             .unwrap();
         let props = props.clone();
         let state_2 = state.clone();
-        let mut last_time = 0.0;
-        let spawn_period = props.lifespan / props.particle_count as f32;
+        let spawn_period = props.lifespan / props.count as f32;
         let mut spawn_credits = 0.0;
         state_2.borrow_mut().callback = Some(Closure::new(move |time: f64| {
-            let delta = ((time - last_time) * 0.001).clamp(0.0, 1.0) as f32;
-            last_time = time;
-            spawn_credits += delta;
             {
                 context.reset();
                 let mut state = state.borrow_mut();
 
-                while spawn_credits > spawn_period {
+                let delta = ((time - state.last_time) * 0.001).clamp(0.0, 0.5) as f32;
+                spawn_credits += delta;
+
+                while if props.continuous {
+                    spawn_credits > spawn_period
+                } else {
+                    state.last_time == 0.0
+                } && state.confetti.len() < props.count
+                {
                     spawn_credits -= spawn_period;
                     state.confetti.push(Fetti::new(&props));
                 }
@@ -99,6 +120,7 @@ pub fn confetti(props: &ConfettiProps) -> Html {
                     .confetti
                     .retain_mut(|fetti| fetti.update(delta, &props, &context));
                 //gloo_console::log!("{}", state.confetti.len());
+                state.last_time = time;
             }
 
             request_animation_frame(state.borrow().callback.as_ref().unwrap());
@@ -169,8 +191,8 @@ impl Fetti {
             y: props.y,
             wobble: rand_unit(),
             wobble_speed: rand_range(0.01, 0.015),
-            velocity: props.start_velocity + (0.5 + 0.5 * js_sys::Math::random() as f32),
-            angle_2d: props.angle + rand_range(-props.spread, props.spread),
+            velocity: props.velocity + (0.5 + 0.5 * js_sys::Math::random() as f32),
+            angle_2d: props.angle + rand_range(-props.spread * 0.5, props.spread * 0.5),
             tilt_angle: rand_max(std::f32::consts::TAU),
             color: props.colors[rand_max(props.colors.len() as f32) as usize],
             shape: props.shapes[rand_max(props.shapes.len() as f32) as usize],
@@ -185,7 +207,7 @@ impl Fetti {
         context: &CanvasRenderingContext2d,
     ) -> bool {
         self.x += (self.angle_2d.cos() * self.velocity + props.drift) * delta;
-        self.y += (self.angle_2d.sin() * self.velocity + props.gravity) * delta;
+        self.y += (self.angle_2d.sin() * self.velocity - props.gravity) * delta;
         self.velocity *= props.decay.powf(delta);
         self.wobble += self.wobble_speed * delta;
         self.tilt_angle += 0.1 * delta;
@@ -199,14 +221,17 @@ impl Fetti {
     }
 
     fn draw(&self, props: &ConfettiProps, context: &CanvasRenderingContext2d) {
-        let wobble_x = self.x + self.wobble.cos() * props.scalar;
-        let wobble_y = self.y + self.wobble.sin() * props.scalar;
+        let center_x = map_ranges(self.x, 0.0..1.0, 0.0..props.width as f32);
+        let center_y = map_ranges(self.y, 0.0..1.0, props.height as f32..0.0);
+
+        let wobble_x = center_x + self.wobble.cos() * props.scalar;
+        let wobble_y = center_y + self.wobble.sin() * props.scalar;
         let tilt_sin = self.tilt_angle.sin();
         let tilt_cos = self.tilt_angle.cos();
 
         let random = rand_range(2.0, 3.0);
-        let x1 = self.x + tilt_cos * random;
-        let y1 = self.y + tilt_sin * random;
+        let x1 = center_x + tilt_cos * random;
+        let y1 = center_y + tilt_sin * random;
         let x2 = wobble_x + tilt_cos * random;
         let y2 = wobble_y + tilt_sin * random;
 
@@ -218,8 +243,8 @@ impl Fetti {
         match self.shape {
             Shape::Circle => {
                 let _ = context.ellipse(
-                    self.x as f64,
-                    self.y as f64,
+                    center_x as f64,
+                    center_y as f64,
                     ((x2 - x1).abs() * 0.5) as f64,
                     ((y2 - y1).abs() * 0.5) as f64,
                     self.wobble as f64,
@@ -228,7 +253,7 @@ impl Fetti {
                 );
             }
             Shape::Square => {
-                context.move_to(self.x.floor() as f64, self.y.floor() as f64);
+                context.move_to(center_x.floor() as f64, center_y.floor() as f64);
                 context.line_to(wobble_x.floor() as f64, y1 as f64);
                 context.line_to(x2.floor() as f64, y2.floor() as f64);
                 context.line_to(x1.floor() as f64, wobble_y.floor() as f64);
@@ -237,5 +262,19 @@ impl Fetti {
 
         context.close_path();
         context.fill();
+    }
+}
+
+#[inline]
+pub fn map_ranges(number: f32, old: Range<f32>, new: Range<f32>) -> f32 {
+    let old_range = old.end - old.start;
+    let new_range = new.end - new.start;
+    let mul: f32 = new_range / old_range;
+    let add: f32 = -old.start * mul + new.start;
+
+    if cfg!(target_feature = "fma") {
+        number.mul_add(mul, add)
+    } else {
+        number * mul + add
     }
 }
